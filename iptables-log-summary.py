124b4e41e3a7286de3d49d67ec74eab062d68783
Rodney Amato <rodnet@gmail.com>
2010-03-22 11:25:39 +1100

    Added a regex for matching udp packet log entries

--- iptables-log-summary.py
+++ iptables-log-summary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # IN= OUT=eth0 SRC=10.11.19.196 DST=10.16.220.132 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=964 DF PROTO=TCP SPT=45179 DPT=3306 WINDOW=5840 RES=0x00 SYN URGP=0
-
+# IN= OUT=eth0 SRC=10.11.19.194 DST=10.6.171.68 LEN=58 TOS=0x00 PREC=0x00 TTL=64 ID=60930 PROTO=UDP SPT=32802 DPT=53 LEN=38
 import re, sys, os
 
 def main():
@@ -14,7 +14,12 @@ def main():
     ports = {}
 
     for line in lines:
+        # TCP log line regex
         matches = re.search("IN=([^ ]*) OUT=([^ ]*) SRC=([^ ]*) DST=([^ ]*) LEN=([^ ]*) TOS=([^ ]*) PREC=([^ ]*) TTL=([^ ]*) ID=([^ ]*) DF PROTO=([^ ]*) SPT=([^ ]*) DPT=([^ ]*) WINDOW=([^ ]*) RES=([^ ]*) SYN URGP=([^ ]*)", line.strip())
+
+        if matches is None:
+            # UDP log line regex
+            matches = re.search("IN=([^ ]*) OUT=([^ ]*) SRC=([^ ]*) DST=([^ ]*) LEN=([^ ]*) TOS=([^ ]*) PREC=([^ ]*) TTL=([^ ]*) ID=([^ ]*) PROTO=([^ ]*) SPT=([^ ]*) DPT=([^ ]*)", line.strip())
 
         if matches is None:
             continue
