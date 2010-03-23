a2a2a13b8a1746d1747675bb7cd1adb156cad752
Rodney Amato <rodnet@gmail.com>
2010-03-23 13:40:19 +1100

    Use the fileinput module for getting the input to process. This gives us greater flexibility, allows stdin or multiple files as sources.

--- iptables-log-summary.py
+++ iptables-log-summary.py
@@ -2,22 +2,17 @@
 
 # IN= OUT=eth0 SRC=10.11.19.196 DST=10.16.220.132 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=964 DF PROTO=TCP SPT=45179 DPT=3306 WINDOW=5840 RES=0x00 SYN URGP=0
 # IN= OUT=eth0 SRC=10.11.19.194 DST=10.6.171.68 LEN=58 TOS=0x00 PREC=0x00 TTL=64 ID=60930 PROTO=UDP SPT=32802 DPT=53 LEN=38
-import re, sys, os
+import re, sys, os, fileinput
 
 def sortData(dict):
     return sorted(dict.keys(), key=lambda x:(x[2], x[1], x[0], x[3]))
 
 def main():
-    if os.path.exists("/var/log/messages"):
-        lines = open("/var/log/messages", "r").readlines()
-    else:
-        lines = sys.stdin.readlines()
-
     ignoredevs = []
     ports = {}
     addr = ""
 
-    for line in lines:
+    for line in fileinput.input():
         # TCP log line regex
         matches = re.search("IN=([^ ]*) OUT=([^ ]*) SRC=([^ ]*) DST=([^ ]*) LEN=([^ ]*) TOS=([^ ]*) PREC=([^ ]*) TTL=([^ ]*) ID=([^ ]*) (?:DF)? PROTO=([^ ]*) SPT=([^ ]*) DPT=([^ ]*) WINDOW=([^ ]*) RES=([^ ]*) SYN URGP=([^ ]*)", line.strip())
 
